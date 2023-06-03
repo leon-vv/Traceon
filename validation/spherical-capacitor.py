adbcbb1cc4a33e770e62efa607328ca428c19525
Léon van Velzen <leonvanvelzen@protonmail.com>
2023-06-03 12:56:23 +0200

    Add 'compute_field' function to validation/spherical-capacitor.py

--- validation/spherical-capacitor.py
+++ validation/spherical-capacitor.py
@@ -55,12 +55,14 @@ def create_geometry(MSF, symmetry):
         geom.set_mesh_size_factor(MSF)
         return geom.generate_mesh()
 
-
-def compute_error(geom):
+def compute_field(geom):
     exc = E.Excitation(geom)
     exc.add_voltage(inner=5/3, outer=3/5)
     
     field = S.solve_bem(exc)
+    return exc, field
+
+def compute_error(exc, field, geom):
     correct = -10/(2/cos(angle)**2 - 1)
     assert -12.5 <= correct <= 7.5 # Between spheres
      
@@ -92,4 +94,4 @@ with the axis is compared with the exact values given in following paper (first 
 Comparison of FDM, FEM and BEM for electrostatic charged particle optics. D. Cubric , B. Lencova, F.H. Read, J. Zlamal. 1999.
 '''
 
-util.parse_validation_args(create_geometry, compute_error, inner='blue', outer='darkblue')
+util.parse_validation_args(create_geometry, compute_field, compute_error, inner='blue', outer='darkblue')
