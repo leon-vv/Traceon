ab07184151ca12f46f24f0709c373d38ddd3ad14
Léon van Velzen <leonvanvelzen@protonmail.com>
2023-07-01 18:22:34 +0200

    Tweak validation/dohi.py

--- validation/dohi.py
+++ validation/dohi.py
@@ -14,9 +14,9 @@ def create_geometry(MSF, symmetry):
      
     revolve_factor = 0.0 if symmetry == G.Symmetry.RADIAL else 1.0
     rmax = 1.0
-    margin = 0.2
+    margin = 0.3
      
-    with G.MEMSStack(z0=-margin, margin=margin, zmin=0.1, zmax=1.6, size_from_distance=True, revolve_factor=revolve_factor, rmax=rmax) as geom:
+    with G.MEMSStack(z0=-margin, margin=margin, zmin=0.1, zmax=1.7, size_from_distance=True, revolve_factor=revolve_factor, rmax=rmax) as geom:
         
         # Close mirror at the bottom, like in the paper
         if symmetry == G.Symmetry.THREE_D:
@@ -39,7 +39,7 @@ def create_geometry(MSF, symmetry):
 
 def compute_field(geom):
     exc = E.Excitation(geom)
-    exc.add_voltage(ground=0.0, mirror=-1250, lens=200)
+    exc.add_voltage(ground=0.0, mirror=-1250, lens=710)
     exc.add_boundary('boundary')
     
     field = S.solve_bem(exc)
@@ -47,11 +47,14 @@ def compute_field(geom):
     return exc, field
 
 def compute_error(exc, field, geom):
-    axial_field = field.axial_derivative_interpolation(0.1, 5.0, 500)
+    axial_field = field.axial_derivative_interpolation(0.05, 1.7, 400)
+
+    bounds = ((-0.1, 0.1), (-0.1, 0.1), (0.05, 1.7)) if geom.symmetry == G.Symmetry.THREE_D else ((-0.1, 0.1), (0.05, 1.7))
+    field.set_bounds(bounds)
      
     bounds = ((-0.1, 0.1), (-0.03, 0.03), (0.05, 19.0)) if geom.symmetry == G.Symmetry.THREE_D else ((-0.03, 0.03), (0.05, 19.0))
     tracer_derivs = T.Tracer(axial_field, bounds)
-    
+     
     angle = 0.5e-3
     z0 = 15
     
