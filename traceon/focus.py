a94be6faf69561ef4ca30a0e47afa1975a61fa24
Léon van Velzen <leonvanvelzen@protonmail.com>
2023-07-09 11:00:01 +0200

    Add traceon/focus.py with now only the function focus_position(...)

---
+++ traceon/focus.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def focus_position(positions):
+    two_d = positions[0].shape[1] == 4
+    
+    # Also for 2D, extend to 3D
+    if two_d:
+        positions = [np.column_stack(
+            (p[:, 0], np.zeros(len(p)), p[:, 1], p[:, 2], np.zeros(len(p)), p[:, 3])) for p in positions]
+      
+    assert all(p.shape == (len(p), 6) for p in positions)
+     
+    angles_x = np.array([p[-1, 3]/p[-1, 5] for p in positions])
+    angles_y = np.array([p[-1, 4]/p[-1, 5] for p in positions])
+    x, y, z = [np.array([p[-1, i] for p in positions]) for i in [0, 1, 2]]
+     
+    N = len(positions)
+    first_column = np.concatenate( (-angles_x, -angles_y) )
+    second_column = np.concatenate( (np.ones(N), np.zeros(N)) )
+    third_column = np.concatenate( (np.zeros(N), np.ones(N)) )
+    right_hand_side = np.concatenate( (x - z*angles_x, y - z*angles_y) )
+     
+    (z, x, y) = np.linalg.lstsq(
+        np.array([first_column, second_column, third_column]).T,
+        right_hand_side, rcond=None)[0]
+
+    return (x, y, z) if not two_d else (x, z)
+    
