3c33aa5eef9fd5847d6ee44854cf529976e2a432
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-03-02 14:13:01 +0100

    Add magnetostatic related validations

--- validation/rectangular_coil.py
+++ validation/rectangular_coil.py
@@ -1,63 +1,57 @@
+import time
+
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.constants import mu_0
 
 import traceon.geometry as G
+import traceon.solver as S
 import traceon.excitation as E
 import traceon.plotting as P
-import traceon.solver as S
+import traceon.tracing as T
 
 from validation import Validation
 
-class MagneticTorus(Validation):
-     
+class RectangularCoil(Validation):
+
     def __init__(self):
-        super().__init__()
-        self.plot_colors = dict(boundary='blue', coil='red', circle='green')
-    
+        super().__init__('Calculate current field of rectangular coil.')
+        self.plot_colors = dict(coil='red', boundary='purple')
+
     def create_mesh(self, MSF, symmetry, higher_order):
         with G.Geometry(G.Symmetry.RADIAL) as geom:
             points = [[0, 5], [5,5], [5,0], [0, 0]]
             lines = [geom.add_line(geom.add_point(p1), geom.add_point(p2)) for p1, p2 in zip(points, points[1:])]
             geom.add_physical(lines, 'boundary')
-            
-            circle = geom.add_circle([2.5, 4], 0.5)
-            geom.add_physical(circle.curve_loop.curves, 'circle')
+             
+            #circle = geom.add_circle([2.5, 4], 0.5)
+            #geom.add_physical(circle.curve_loop.curves, 'circle')
             
             geom.set_mesh_size_factor(MSF)
             mesh1 = geom.generate_line_mesh(higher_order)
-        
+
         with G.Geometry(G.Symmetry.RADIAL) as geom:
             rect = geom.add_rectangle(2, 3, 2, 3, 0)
             geom.add_physical(rect.surface, 'coil')
-             
             geom.set_mesh_size_factor(MSF)
             mesh2 = geom.generate_triangle_mesh(higher_order)
 
         return mesh1 + mesh2
     
-    def supports_fmm(self):
-        return False
-
-    def supports_3d(self):
-        return False
-    
-    def get_excitation(self, geometry):
-        exc = E.Excitation(geometry)
+    def get_excitation(self, mesh):
+        exc = E.Excitation(mesh)
         exc.add_current(coil=1)
-        exc.add_magnetizable(circle=10)
         exc.add_boundary('boundary')
         return exc
-    
-    def correct_value_of_interest(self):
-        return 154.54131081/1e4
      
-    def compute_value_of_interest(self, geometry, field):
-        result = field.magnetostatic_field_at_point(np.array([2.5, 4]))[0]
-        return result
+    def correct_value_of_interest(self):
+        cr, cz = 0.001152863994, 2.647794398E-4 # Correct
+        return cr/(1e4*mu_0)
+     
+    def compute_value_of_interest(self, geom, field):
+        fr, fz = field.magnetostatic_field_at_point(np.array([2.5, 4]))
+        return fr
 
 if __name__ == '__main__':
-    MagneticTorus().run_validation()
-
-
-
+    RectangularCoil().run_validation()
 
---
+++ validation/two_current_coils.py
@@ -0,0 +1,69 @@
+import os.path as path
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.interpolate import CubicSpline
+
+import traceon.geometry as G
+import traceon.excitation as E
+import traceon.plotting as P
+import traceon.solver as S
+
+from validation import Validation
+
+class TwoCurrentCoils(Validation):
+     
+    def __init__(self):
+        super().__init__('Calculate magnetic field of two current coils in presence of rectangular dielectric')
+        self.plot_colors = dict(coil1='blue', coil2='red', block='orange')
+    
+    def create_mesh(self, MSF, symmetry, higher_order):
+        with G.Geometry(G.Symmetry.RADIAL) as geom:
+            
+            circle1 = geom.add_circle([10e-3, 5e-3], 1.0e-3)
+            circle2 = geom.add_circle([10e-3, -5e-3], 1.0e-3)
+             
+            geom.add_physical(circle1.plane_surface, 'coil1')
+            geom.add_physical(circle2.plane_surface, 'coil2')
+            
+            geom.set_mesh_size_factor(10*MSF)
+            mesh1 = geom.generate_triangle_mesh(higher_order)
+
+        with G.Geometry(G.Symmetry.RADIAL) as geom:
+            rectangle = geom.add_rectangle(5e-3, 15e-3, -1e-3, 1e-3, 0)
+            geom.add_physical(rectangle.curves, 'block')
+            
+            points = [[0, 50e-3], [100e-3, 50e-3], [100e-3, -50e-3], [0, -50e-3]]
+            lines = [geom.add_line(geom.add_point(p1), geom.add_point(p2)) for p1, p2 in zip(points, points[1:])]
+            geom.add_physical(lines, 'boundary')
+             
+            geom.set_mesh_size_factor(MSF)
+            mesh2 = geom.generate_line_mesh(higher_order)
+        
+        return mesh1 + mesh2
+    
+    def supports_fmm(self):
+        return False
+    
+    def supports_3d(self):
+        return False
+    
+    def get_excitation(self, geometry):
+        exc = E.Excitation(geometry)
+        exc.add_current(coil1= 10/(np.pi*(1e-3)**2), coil2=-10/(np.pi*(1e-3)**2))
+        exc.add_magnetizable(block=25)
+        exc.add_boundary('boundary')
+        return exc
+    
+    def correct_value_of_interest(self):
+        return -91.94907464785867
+    
+    def compute_value_of_interest(self, geometry, field):
+        return field.magnetostatic_field_at_point(np.array([10e-3, 0.]))[0]
+
+if __name__ == '__main__':
+    TwoCurrentCoils().run_validation()
+
+
+
+
