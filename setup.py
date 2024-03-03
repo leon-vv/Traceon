260318490b113c41257bee561ef27d58b15f5b4d
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-03-03 15:18:52 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ backend_extension = Extension(
 
 setup(
     name='traceon',
-    version='0.4.0',
+    version='0.5.0',
     description='Solver and tracer for electrostatic problems',
     url='https://github.com/leon-vv/Traceon',
     author='Léon van Velzen',
