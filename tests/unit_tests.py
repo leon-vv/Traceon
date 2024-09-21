f237b2ac2b267d70fe6e623ea7be4508566bcae4
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-09-21 16:36:40 +0200

    Add test_position_and_jacobian_radial_length

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -579,6 +579,49 @@ class TestBackend(unittest.TestCase):
         _, pos = B.position_and_jacobian_radial(1, *line)
         assert np.all(np.isclose(pos, line[3, :2]))
     
+    def test_position_and_jacobian_radial_length(self):
+        line = np.array([
+            [0.0, 0.0, 0.0],
+            [0.5, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [1.5, 0.0, 0.0]])
+        
+        length = quad(lambda x: B.position_and_jacobian_radial(x, *line)[0], -1, 1)[0]
+        assert np.isclose(length, 1.5)
+        
+        line = np.array([
+            [0.0, 0.0, 0.0],
+            [1.5, 0.0, 0.0],
+            [0.5, 0.0, 0.0],
+            [1.0, 0.0, 0.0]])
+        
+        jac, pos = B.fill_jacobian_buffer_radial(np.array([line]))
+        assert np.isclose(np.sum(jac[0]), 1.5)
+
+        line = np.array([[ 2.1       , -0.74974852,  0.        ],
+            [ 2.1       , -0.77751699,  0.        ],
+            [ 2.1       , -0.80528545,  0.        ],
+            [ 2.1       , -0.83305391,  0.        ]])
+             
+        length = quad(lambda x: B.position_and_jacobian_radial(x, *line)[0], -1, 1)[0]
+        assert np.isclose(length, line[0, 1] - line[3, 1])
+
+        normal = np.array([-1., 0., 0.])
+        middle = np.mean(line, axis=0)
+
+        def field_dot_normal_integrand(x):
+            jac, pos = B.position_and_jacobian_radial(x, *line)
+            
+            field = np.array([
+                -B.dr1_potential_radial_ring(middle[0], middle[1], pos[0], pos[1]),
+                -B.dz1_potential_radial_ring(middle[0], middle[1], pos[0], pos[1]),
+                0.0])
+
+            return jac * np.dot(normal, field)
+        
+        field_dot_normal = quad(field_dot_normal_integrand, -1, 1, points=[-1., 0., 1.])[0]
+        assert np.isclose(field_dot_normal, -0.01893954382812056)
+    
     def test_plane_intersection(self):
         p = np.array([
             [3, 0, 0, 0, 0, 0],
