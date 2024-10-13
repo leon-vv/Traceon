4dd4e405566e0aa9c609f937badfc2524fca0387
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-10-14 00:29:31 +0200

    Split out tracing.c from traceon-backend.c

---
+++ traceon/backend/tracing.c
@@ -0,0 +1,117 @@
+
+
+#define TRACING_STEP_MAX 0.01
+
+EXPORT const size_t TRACING_BLOCK_SIZE = (size_t) 1e5;
+
+// python -c "from scipy.constants import m_e, e; print(-e/m_e);"
+const double EM = -175882001077.2163; // Electron charge over electron mass
+// python -c "from scipy.constants import mu_0; print(mu_0);"
+const double MU_0 = 1.25663706212e-06;
+
+const double A[]  = {0.0, 2./9., 1./3., 3./4., 1., 5./6.};	// https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta%E2%80%93Fehlberg_method
+const double B6[] = {65./432., -5./16., 13./16., 4./27., 5./144.};
+const double B5[] = {-17./12., 27./4., -27./5., 16./15.};
+const double B4[] = {69./128., -243./128., 135./64.};
+const double B3[] = {1./12., 1./4.};
+const double B2[] = {2./9.};
+const double CH[] = {47./450., 0., 12./25., 32./225., 1./30., 6./25.};
+const double CT[] = {-1./150., 0., 3./100., -16./75., -1./20., 6./25.};
+
+typedef void (*field_fun)(double pos[6], double field[3], void* args);
+
+void
+produce_new_y(double y[6], double ys[6][6], double ks[6][6], size_t index) {
+	
+	const double* coefficients[] = {NULL, B2, B3, B4, B5, B6};
+	
+	for(int i = 0; i < 6; i++) {
+		
+		ys[index][i] = y[i];
+		
+		for(int j = 0; j < index; j++) 
+			ys[index][i] += coefficients[index][j]*ks[j][i];
+	}
+}
+
+void
+produce_new_k(double ys[6][6], double ks[6][6], size_t index, double h, field_fun ff, void *args) {
+	
+	double field[3] = { 0. };
+	ff(ys[index], field, args);
+	
+	ks[index][0] = h*ys[index][3];
+	ks[index][1] = h*ys[index][4];
+	ks[index][2] = h*ys[index][5];
+	ks[index][3] = h*EM*field[0];
+	ks[index][4] = h*EM*field[1];
+	ks[index][5] = h*EM*field[2];
+}
+
+
+EXPORT size_t
+trace_particle(double *times_array, double *pos_array, field_fun field, double bounds[3][2], double atol, void *args) {
+	
+	double (*positions)[6] = (double (*)[6]) pos_array;
+	
+	double y[6];
+	for(int i = 0; i < 6; i++) y[i] = positions[0][i];
+	
+    double V = norm_3d(y[3], y[4], y[5]);
+    double hmax = TRACING_STEP_MAX/V;
+    double h = hmax;
+	
+    int N = 1;
+		
+    double xmin = bounds[0][0], xmax = bounds[0][1];
+	double ymin = bounds[1][0], ymax = bounds[1][1];
+	double zmin = bounds[2][0], zmax = bounds[2][1];
+
+	 
+    while( (xmin <= y[0]) && (y[0] <= xmax) &&
+		   (ymin <= y[1]) && (y[1] <= ymax) &&
+		   (zmin <= y[2]) && (y[2] <= zmax) ) {
+		
+		double k[6][6] = { {0.} };
+		double ys[6][6] = { {0.} };
+		
+		for(int index = 0; index < 6; index++) {
+			produce_new_y(y, ys, k, index);
+			produce_new_k(ys, k, index, h, field, args);
+		}
+		
+		double max_position_error = 0.0;
+		double max_velocity_error = 0.0;
+
+		for(int i = 0; i < 3; i++) {
+			double err = 0.0;
+			for(int j = 0; j < 6; j++) err += CT[j]*k[j][i];
+			if(fabs(err) > max_position_error) max_position_error = fabs(err);
+		}
+		
+		for(int i = 3; i < 6; i++) {
+			double err = 0.0;
+			for(int j = 0; j < 6; j++) err += CT[j]*k[j][i];
+			if(fabs(err) > max_velocity_error) max_velocity_error = fabs(err);
+		}
+		
+		double error = max_position_error + h*max_velocity_error;
+			
+		if(error <= atol) {
+			for(int i = 0; i < 6; i++) {
+				y[i] += CH[0]*k[0][i] + CH[1]*k[1][i] + CH[2]*k[2][i] + CH[3]*k[3][i] + CH[4]*k[4][i] + CH[5]*k[5][i];
+				positions[N][i] = y[i];
+				times_array[N] = times_array[N-1] + h;
+			}
+				
+			N += 1;
+			if(N==TRACING_BLOCK_SIZE) return N;
+		}
+		
+		h = fmin(0.9 * h * pow(atol / error, 0.2), hmax);
+	}
+		
+	return N;
+}
+
+
