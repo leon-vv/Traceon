3f2f2d99a2a8fa916aa99f788cfea5268e895f13
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-10-15 20:03:39 +0200

    Add function test_field_radial

--- tests/test_radial.py
+++ tests/test_radial.py
@@ -2,8 +2,11 @@ import unittest
 from math import pi, sqrt
 
 import numpy as np
+from scipy.integrate import quad
+from scipy.constants import epsilon_0
 
 import traceon.backend as B
+from tests.test_radial_ring import potential_of_ring_arbitrary
 
 class TestRadial(unittest.TestCase):
     def test_charge_radial_vertical(self):
@@ -41,3 +44,38 @@ class TestRadial(unittest.TestCase):
         approx = B.charge_radial(vertices, 1.0);
 
         assert np.isclose(correct, approx)
+
+    def test_field_radial(self):
+        vertices = np.array([
+            [1.0, 1.0, 0.0],
+            [2.0, 2.0, 0.0],
+            [1.0 + 1/3, 1.0 + 1/3, 0.0],
+            [1.0 + 2/3, 1.0 + 2/3, 0.0]])
+        
+        r0, z0 = 2.0, -2.5
+        
+        delta = 1e-5
+        
+        def Er(r, z):
+            dVdr = (potential_of_ring_arbitrary(1.0, r0 + delta, z0, r, z)
+                        - potential_of_ring_arbitrary(1.0,r0 - delta, z0, r, z))/(2*delta)
+            return -dVdr
+        
+        def Ez(r, z):
+            dVdz = (potential_of_ring_arbitrary(1.0, r0, z0 + delta, r, z)
+                        - potential_of_ring_arbitrary(1.0, r0, z0 - delta, r, z))/(2*delta)
+            return -dVdz
+        
+        length = sqrt(2)
+        Er = quad(lambda x: Er(1.0 + x, 1.0 + x), 0.0, 1.0)[0] * length
+        Ez = quad(lambda x: Ez(1.0 + x, 1.0 + x), 0.0, 1.0)[0] * length
+
+        jac, pos = B.fill_jacobian_buffer_radial(np.array([vertices]))
+        charges = np.ones(len(jac))
+        assert np.allclose(B.field_radial(np.array([r0, z0]), charges, jac, pos)/epsilon_0, [Er, Ez], atol=0.0, rtol=1e-10)
+    
+        
+
+        
+    
+
