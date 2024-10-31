2e0dad80f9a55b05b7a36740a1c09fea5f0bbcf3
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-10-31 10:29:48 +0100

    Fix bug in function get_number_of_threads

--- traceon/util.py
+++ traceon/util.py
@@ -33,22 +33,24 @@ def get_number_of_threads():
     
     threads = os.environ.get('TRACEON_THREADS')
 
-    if threads is None:
-        # Use all available physical CPU's
-        # To really count the number of physical cores, we would need
-        # a module like psutil. But I don't want to pull in an external
-        # dependency for such a triviality. Here we simply assume that
-        # we have two threads per logical core, which I think is correct
-        # for at least modern Intel and AMD CPU's.
-        cpu_count = len(os.sched_getaffinity(0)) if hasattr(os, 'sched_getaffinity') else os.cpu_count()
-        threads = cpu_count // 2
-    
+    if threads is not None:
+        return int(threads)
+
+    # Use all available physical CPU's
+    # To really count the number of physical cores, we would need
+    # a module like psutil. But I don't want to pull in an external
+    # dependency for such a triviality. 
+    cpu_count = len(os.sched_getaffinity(0)) if hasattr(os, 'sched_getaffinity') else os.cpu_count()
+     
     # os.cpu_count() might be 1 on old CPU's and in virtual machines.
     # Of course at least one thread is needed to run the computations.
-    if threads == 0:
+    if cpu_count is None or cpu_count <= 1:
         return 1
-     
-    return threads
+    
+    # Here we simply assume that
+    # we have two threads per logical core, which I think is correct
+    # for at least modern Intel and AMD CPU's.
+    return cpu_count // 2
 
 def split_collect(f, array):
     
