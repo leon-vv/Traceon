ccbe85c342cbd6c7b3d06c001063cccbb0b055c5
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-11-09 10:49:56 +0100

    Add TestUtilities3D.test_position_and_jacobian

--- tests/test_utilities_3d.py
+++ tests/test_utilities_3d.py
@@ -8,6 +8,24 @@ import traceon.backend as B
 
 class TestUtilities3D(unittest.TestCase):
     
+    def test_position_and_jacobian(self):
+        tri = np.array([
+            [1.0, 1.0, 1.0],
+            [2.0, 0.0, 0.0],
+            [0.0, 3.0, 0.0]])
+        
+        v0, v1, v2 = tri 
+        
+        vec1 = v1 - v0
+        vec2 = v2 - v0
+        
+        alpha = 1/3
+        beta = 1/4
+         
+        _, pos = B.position_and_jacobian_3d(alpha, beta, tri)
+        
+        assert np.allclose(pos, v0 + vec1*alpha + beta*vec2)
+     
     def test_combine_elec_magnetic(self):
         
         for i in range(20):
