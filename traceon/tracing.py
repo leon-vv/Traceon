fixed error in relativstic speed calculation

traceon/tracing.py
@@ -94,7 +94,7 @@ def _convert_velocity_to_SI(velocity, mass):
     speed_eV = np.linalg.norm(velocity)
     kin_E_joule = speed_eV*e
     rest_E_joule = mass*c**2
-    gamma_factor = sqrt(1 - (rest_E_joule/(kin_E_joule**2 + rest_E_joule))**2)
+    gamma_factor = sqrt(1 - (rest_E_joule/(kin_E_joule + rest_E_joule))**2)
     speed = c*gamma_factor
     direction = velocity / speed_eV
     return speed * direction