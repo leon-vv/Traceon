313136b08999d21dab151152a220a55d5a0d667b
Léon van Velzen <leonvanvelzen@protonmail.com>
2024-12-27 16:22:07 +0100

    Remove Field3D_BEM class and potential_3d function

--- traceon/backend/three_d_point.c
+++ traceon/backend/three_d_point.c
@@ -1,10 +1,4 @@
 
-
-INLINE double potential_3d_point(double x0, double y0, double z0, double x, double y, double z, void *_) {
-	double r = norm_3d(x-x0, y-y0, z-z0);
-    return 1/(4*M_PI*r);
-}
-
 EXPORT double dx1_potential_3d_point(double x0, double y0, double z0, double x, double y, double z, void *_) {
 	double r = norm_3d(x-x0, y-y0, z-z0);
     return (x-x0)/(4*M_PI*pow(r, 3));
