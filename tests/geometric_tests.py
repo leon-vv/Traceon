0a20a36e45697575dab8a826b84244d7117c2346
jobdewitte <job.de.witte@zeelandnet.nl>
2025-01-06 22:33:26 +0100

    unittests for geometric properties of field classes

--- tests/geometric_tests.py
+++ tests/geometric_tests.py
@@ -5,9 +5,9 @@ from math import *
 
 import traceon.mesher as M
 from traceon.geometry import *
-import traceon.plotting as P
-
-
+import traceon.excitation as E
+import traceon.solver as S
+from traceon.field import FieldRadialAxial
 
 class MeshTests(unittest.TestCase):
 
@@ -243,13 +243,107 @@ class SurfaceTests(unittest.TestCase):
          
         assert len(mesh.physical_to_triangles['test']) == len(mesh.triangles)
         
+class FieldTests(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        # simple field; details are not important for geometric tests
+        pos = Path.rectangle_xz(0.1,1,1, 1.5)
+        neg = Path.rectangle_xz(0.1,1,-1.5, -1)
+        neg.name='neg'
+        pos.name='pos'
 
+        mesh = (neg + pos).mesh(mesh_size=1)
 
+        excitation = E.Excitation(mesh, E.Symmetry.RADIAL)
+        excitation.add_voltage(neg=-1, pos=1)
+        excitation.add_magnetostatic_potential(neg=-1, pos=1)
+        cls.field = S.solve_direct(excitation)
+        cls.field_axial = FieldRadialAxial(cls.field, -2, 2, 100)
 
+    def test_map_points(self):
+        # translation
+        field_trans = self.field.move(dx=1)
+        # origin should shift
+        assert np.allclose(field_trans.origin, [1.,0.,0.]) 
+        # basis vectors should remain invariant
+        assert np.allclose(field_trans.basis, self.field.basis) 
+        # in homogenous coords the tranformation matrix is [[R, t], [0,1]]
+        assert np.allclose(
+            np.linalg.inv(field_trans.inverse_transformation_matrix), np.array([[1,0,0,1], 
+                                                                                [0,1,0,0],
+                                                                                [0,0,1,0],
+                                                                                [0,0,0,1]]))
+        #rotation after translation
+        field_trans_rot = field_trans.rotate(Ry=np.pi/2)
+        # local origin [1,0,0] rotates as well in global coordinate system
+        assert np.allclose(field_trans_rot.origin, [0,0,-1]) 
+        # x-> -z, y-> y, z-> -x
+        assert np.allclose(field_trans_rot.basis, np.array([[0,0,-1],[0,1,0],[1,0,0]])) 
+        #T_total = T_rot @ T_trans
+        assert np.allclose(
+            np.linalg.inv(field_trans_rot.inverse_transformation_matrix), np.array([[0,0,-1,0], 
+                                                                                    [0,1,0,0],
+                                                                                    [1,0,0,-1],
+                                                                                    [0,0,0,1]]))
+   
+    def test_map_points_to_local(self):
+        field_trans = self.field.move(dx=1)
+        point = np.array([1,1,1])
+        point_trans = point + np.array([1,0,0]) # point translated over same distance as field
+        #translated point should be original point in translated system
+        assert np.allclose(field_trans.map_points_to_local(point_trans), point)
 
+        # origin should locally always coincide with standard origin
+        field_trans_rot = field_trans.rotate(Ry=np.pi/2)
+        assert np.allclose(field_trans_rot.map_points_to_local(field_trans_rot.origin), self.field.origin)
+        # NOTE: map_points_to_local does not work for basis as they are direction vectors not points
+    
+    def test_field_axial_coordinate_system(self):
+        field_axial_trans_rot1 = self.field_axial.move(dz=1).rotate(Rz=np.pi)
 
+        field_trans_rot = self.field.move(dz=1).rotate(Rz=np.pi)
+        field_axial_trans_rot2= FieldRadialAxial(field_trans_rot, -2, 2, 100)
+        
+        # it should not matter wheter we interpolate or transform first
+        # FieldAxial should inherit the coordinate system of its base field
+        assert np.allclose(field_axial_trans_rot1.inverse_transformation_matrix, 
+                           field_axial_trans_rot2.inverse_transformation_matrix)
+    
+    def test_potential_at_point(self):
+        field_trans = self.field.move(dx=1)
+        field_axial_trans = self.field_axial.move(dx=1)
 
+        pg = np.array([1,1,1])
+        pl = field_trans.map_points_to_local(pg)
 
+        assert np.allclose(field_trans.electrostatic_potential_at_point(pg),
+                        field_trans.electrostatic_potential_at_local_point(pl))
+        
+        assert np.allclose(field_trans.magnetostatic_potential_at_point(pg),
+                        field_trans.magnetostatic_potential_at_local_point(pl))
+        
+        assert np.allclose(field_axial_trans.electrostatic_potential_at_point(pg),
+                        field_axial_trans.electrostatic_potential_at_local_point(pl))
+        
+        assert np.allclose(field_axial_trans.magnetostatic_potential_at_point(pg),
+                        field_axial_trans.magnetostatic_potential_at_local_point(pl))
+        
+    def test_field_at_point(self):
+        field_trans = self.field.move(dx=1)
+        field_axial_trans = self.field_axial.move(dx=1)
 
+        pg = np.array([1,1,1])
+        pl = field_trans.map_points_to_local(pg)
 
+        assert np.allclose(field_trans.electrostatic_field_at_point(pg),
+                        field_trans.electrostatic_field_at_local_point(pl))
+        
+        assert np.allclose(field_trans.magnetostatic_field_at_point(pg),
+                        field_trans.magnetostatic_field_at_local_point(pl))
+        
+        assert np.allclose(field_axial_trans.electrostatic_field_at_point(pg),
+                        field_axial_trans.electrostatic_field_at_local_point(pl))
+        
+        assert np.allclose(field_axial_trans.magnetostatic_field_at_point(pg),
+                        field_axial_trans.magnetostatic_field_at_local_point(pl))
