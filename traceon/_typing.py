87a990c4aebb736955d6836740e1714be7099188
jobdewitte <job.de.witte@zeelandnet.nl>
2025-01-21 11:53:33 +0100

    added indices alias

--- traceon/_typing.py
+++ traceon/_typing.py
@@ -26,13 +26,13 @@ from numpy.typing import NDArray
 
 
 Point3D: TypeAlias = NDArray[np.floating]  # Single point as a (3,) array
-Point3DLike: TypeAlias = Point3D | Sequence[np.floating]  # Single point as (3,) array or sequence
+Point3DLike: TypeAlias = Point3D | Sequence[float]  # Single point as (3,) array or sequence
 
 Points: TypeAlias = NDArray[np.floating]  # Multiple points as (N, 3) array
 PointsLike: TypeAlias = Points | Sequence[Point3DLike]  # (N, 3) array or sequence of Point3DLike
 
 Vector3D: TypeAlias = NDArray[np.floating]  # Single vector as a (3,) array
-Vector3DLike: TypeAlias = Vector3D | Sequence[np.floating]  # Single vector as (3,) array or sequence
+Vector3DLike: TypeAlias = Vector3D | Sequence[float]  # Single vector as (3,) array or sequence
 
 Vectors: TypeAlias = NDArray[np.floating]  # Multiple vectors as (N, 3) array
 VectorsLike: TypeAlias = Vectors | Sequence[Vector3DLike]  # (N, 3) array or sequence of Vector3DLike
@@ -57,7 +57,14 @@ QuadLike: TypeAlias = Quad | Sequence[int]
 Quads: TypeAlias = NDArray[np.integer]
 QuadsLike: TypeAlias = Quads | Sequence[QuadLike]
 
+Indices = NDArray[np.integer]
+IndicesLike = Indices | Sequence[int]
+
+
+Jacobian = NDArray[np.floating]
+Jacobians = NDArray[np.floating]
 if TYPE_CHECKING:
-    from .geometry import (
-        Surface
-    )
+    from .geometry import Path, PathCollection, Surface, SurfaceCollection
+    from .mesher import Mesh
+    from .excitation import Excitation
+    from .field import EffectivePointCharges, Field, FieldBEM, FieldRadialBEM
